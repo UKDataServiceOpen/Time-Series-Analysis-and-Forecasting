890e96a36ec193106b05c24337520e5c0a9932f5
NadiaK98 <nakennar@gmail.com>
2023-03-29 17:18:05 +0100

    Create install.r

---
+++ install.r
@@ -0,0 +1,1 @@
+install.packages(c(("dplyr", "readr", "tibble", "janitor", "fable", "tsibble", "TTR", "lubridate", "tseries", "ggfortify", "ggplo2", "forecast", "crimedata", "zoo", "xtable", "xts"))
